0da6b7455d87be83c9f026978a38df4155678a3d
Björn Arnelid <bjorn.arnelid@gmail.com>
2019-10-11 19:45:54 +0200

    Add arguments

--- s4s.py
+++ s4s.py
@@ -1,12 +1,19 @@
 # Main Launcher script
 
+
+import argparse
 import historic_temp
 
 
 if __name__ == "__main__":
-    temp = historic_temp.HistoricTemperatures()
-    temp.collect_data()
-    temp.load()
-    temp.plot()
-    temp.verify()
+    parser = argparse.ArgumentParser(description='Scripts 4 Sceptics. Verify things for yourself!')
+    parser.add_argument('script', metavar='SCRIPT', type=str, help='script to verify',
+                        choices=['historic_temp'])
+    args = parser.parse_args()
+    if args.script == 'historic_temp':
+        temp = historic_temp.HistoricTemperatures()
+        temp.collect_data()
+        temp.load()
+        temp.plot()
+        temp.verify()
 
